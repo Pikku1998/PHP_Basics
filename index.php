5eed271f2a80d05be57519c206ea43aa3a7ec066
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-11 13:04:54 +0530

    comments, html in php file

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
-    echo "Hello World"
+    echo "Hello World";
+    echo "<br>Hi Prakash";
+    // single line comment
+
+    /* 
+    multi
+    line
+    comment
+    */
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>a button</button>
+    
+</body>
+</html>
