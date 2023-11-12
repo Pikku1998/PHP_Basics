66feab6721acca2fa001153b1e725eeb7786a315
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-12 23:04:05 +0530

    Connecting Database using mysqli extension.

---
+++ database.php
@@ -0,0 +1,14 @@
+<?php
+    $host='localhost';
+    $user = 'root';
+    $pass = 'asdfgh@1' ;
+    $db_name = 'php_db';
+
+    try{
+        $connection = mysqli_connect($host, $user, $pass, $db_name);
+        echo 'Connected';
+    }
+    catch(mysqli_sql_exception){
+        echo 'Connection Failed';
+    }
+?>
--- index.php
+++ index.php
@@ -1,13 +1,3 @@
 <?php
-    $password = 'Prakash';
-    $hash = password_hash($password, PASSWORD_BCRYPT);
-
-    echo $hash,"<br>";
-
-    if(password_verify('Prakash', $hash)){
-        echo 'Logged In.<br>';
-    }
-    else{
-        echo 'Invalid';
-    }
+    include('database.php');
 ?>
