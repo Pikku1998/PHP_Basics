02bae1c54da9c0f935d672df9ce31d7e645fd9fa
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-12 17:33:41 +0530

    String functions

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
-function birthday($name, $age){
-    echo "Happy Birthday {$name}<br>";
-    echo "Happy Birthday to you<br>";
-    echo "You are {$age} years old<br>";
-    echo "<br>";
+$name = 'Prakash R';
+$number = '123-456-7890';
+
+echo strtolower($name),'<br>';
+echo strtoupper($name),'<br>';
+echo strrev($name),'<br>';
+echo str_shuffle($name),'<br>';
+echo str_pad($name,20,'-'),'<br>';
+echo str_replace('-','',$number),'<br>';
+echo strcmp($name, 'Eswar'),'<br>';  // returns 1 if false
+echo strcmp($name, 'Prakash R'),'<br>';  // returns 0 if true
+echo strlen($number),'<br>';
+echo strpos($number, '-'),'<br>';
+echo substr($name,3,4,),'<br>'; // (string, start_position, length)
+
+$number_bits = explode('-',$number); // (seperator,string) splits string into array
+foreach($number_bits as $bit){
+    echo $bit.'<br>';
 }
-birthday('Prakash',25);
-birthday('Eswar', 23);
-birthday(54,54);
 
-
-function hypotenuse(int $a,int $b){
-    return sqrt($a**2 + $b**2);
-}
-echo hypotenuse(5,4),"<br>";
-
-
-function is_even(int $number){
-    if($number%2 == 0){
-        return true;
-    }
-    return false;
-}
-echo is_even(3),"<br>";
-echo is_even(4),"<br>";
+$name = array("Amma","Prakash", "Eswar");
+echo implode(" ",$name);
 
 ?>
