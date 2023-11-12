5b295d982a7dcf035f8901e238a1ca52daab2f31
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-12 22:21:36 +0530

    password hashing

--- index.php
+++ index.php
@@ -1,35 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-</head>
-<body>
-    <h3>Login page</h3>
-    <form action="<?php htmlspecialchars($_SERVER['PHP_SELF'])?>" method="post">
-        <input type="text" name="user" placeholder="username">
-        <br>
-        <input type="password" name="password" placeholder="password">
-        <br>
-        <input type="submit" name='login' value="Login">
-        
-    </form>
-    <br>
-</body>
-</html>
-
 <?php
-// echo $_SERVER['PHP_SELF'],'<br>';
-// echo $_SERVER["REQUEST_METHOD"];
+    $password = 'Prakash';
+    $hash = password_hash($password, PASSWORD_BCRYPT);
 
-    if($_SERVER["REQUEST_METHOD"] == "POST"){
-        echo "Hi";
+    echo $hash,"<br>";
+
+    if(password_verify('Prakash', $hash)){
+        echo 'Logged In.<br>';
     }
-
-    
-
-    foreach($_SERVER as $key=>$value){
-        echo "{$key} = {$value}<br>";
+    else{
+        echo 'Invalid';
     }
 ?>
