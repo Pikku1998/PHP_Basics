8d6bf3e9426110df2549cccb96a6ec342601f57b
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-12 16:10:47 +0530

    isset() and empty() functions

--- index.php
+++ index.php
@@ -1,61 +1,18 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="post">
-        <!-- <input type="radio" name="food" value='Biryani'>Biryani
-        <br>
-        <input type="radio" name="food" value='Burger'>Burger
-        <br>
-        <input type="submit" name='confirm' value="Confirm">
-        <br>
-        <br> -->
-        <input type="checkbox" name="curry[]" value='Chicken'>Chicken
-        <br>
-        <input type="checkbox" name="curry[]" value='Fish'>Fish
-        <br>
-        <input type="submit" name='confirm'>
-
-
-
-    </form>
-</body>
-</html>
-
 <?php
-    // if(isset($_POST['confirm'])){
-    //     if(isset($_POST['food'])){
-    //         $food = $_POST['food'];
-    //         echo "You selected {$food}";
-    //     }
-    //     else{
-    //         echo 'please make a selection.';
-    //     };
-    // };
-    
-    if(isset($_POST['confirm']))
-    {
-        if(isset($_POST['curry']))
-        {
-            $curries = $_POST['curry'];
-            foreach($curries as $curry)
-            {
-                echo $curry,'<br>';
-            }
-        }
-        else{
-            echo 'Select something.';
-        }
+$name = 'hello';
+$number = null;
+$email = '';
 
-    };
+echo 'isset() values<br>';
+echo isset($name),'<br>';
+echo isset($number),'<br>';
+echo isset($email),'<br>';
 
-    // foreach($curries as $curry){
-    //     echo $curry;
-    // }
+echo '<br>';
 
+echo 'empty() values<br>';
+echo empty($name),'<br>';
+echo empty($number),'<br>';
+echo empty($email),'<br>';
 
 ?>
