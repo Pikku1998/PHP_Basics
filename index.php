105961e83dbcf0092cbd246b79f05c188accb481
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-12 16:39:19 +0530

    functions

--- index.php
+++ index.php
@@ -1,18 +1,28 @@
 <?php
-$name = 'hello';
-$number = null;
-$email = '';
+function birthday($name, $age){
+    echo "Happy Birthday {$name}<br>";
+    echo "Happy Birthday to you<br>";
+    echo "You are {$age} years old<br>";
+    echo "<br>";
+}
+birthday('Prakash',25);
+birthday('Eswar', 23);
+birthday(54,54);
 
-echo 'isset() values<br>';
-echo isset($name),'<br>';
-echo isset($number),'<br>';
-echo isset($email),'<br>';
 
-echo '<br>';
+function hypotenuse(int $a,int $b){
+    return sqrt($a**2 + $b**2);
+}
+echo hypotenuse(5,4),"<br>";
 
-echo 'empty() values<br>';
-echo empty($name),'<br>';
-echo empty($number),'<br>';
-echo empty($email),'<br>';
+
+function is_even(int $number){
+    if($number%2 == 0){
+        return true;
+    }
+    return false;
+}
+echo is_even(3),"<br>";
+echo is_even(4),"<br>";
 
 ?>
