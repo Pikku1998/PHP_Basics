cce099894d3f0c4b02fc713207d61d35901786af
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-13 11:09:03 +0530

    querying Database - INSERT

--- database.php
+++ database.php
@@ -1,14 +1,14 @@
 <?php
     $host='localhost';
     $user = 'root';
-    $pass = 'asdfgh@1' ;
+    $pass = 'asdfgh@1';
     $db_name = 'php_db';
 
     try{
         $connection = mysqli_connect($host, $user, $pass, $db_name);
-        echo 'Connected';
+        echo 'Connected<br>';
     }
     catch(mysqli_sql_exception){
-        echo 'Connection Failed';
+        echo 'Connection Failed<br>';
     }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
     include('database.php');
+
+    $name = 'Eswar';
+    $password = 'Eswar';
+    $hash = password_hash($password, PASSWORD_BCRYPT);
+
+
+    $query = "INSERT INTO users (name, password) VALUES ('$name', '$hash')";
+    try{
+        mysqli_query($connection, $query);
+        echo 'User is registered<br>';
+    }
+    catch(mysqli_sql_exception){
+        echo 'Could not register user.<br>';
+    }
+
+    mysqli_close($connection);
+
 ?>
