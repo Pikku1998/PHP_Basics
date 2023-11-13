0b76f907d5286332814bd3e2c8ac0e738ccc1084
Prakash R <prakashramanukolanu1998@gmail.com>
2023-11-13 11:23:17 +0530

    querying database - SELECT

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
     include('database.php');
 
-    $name = 'Eswar';
-    $password = 'Eswar';
-    $hash = password_hash($password, PASSWORD_BCRYPT);
 
 
-    $query = "INSERT INTO users (name, password) VALUES ('$name', '$hash')";
-    try{
-        mysqli_query($connection, $query);
-        echo 'User is registered<br>';
+    $query = "SELECT * FROM users WHERE name='Eswar'";
+
+
+    $query_result = mysqli_query($connection, $query); // executes query and returns a query object.
+
+    if(mysqli_num_rows($query_result) > 0){  // gives number of rows available in query object
+        while($rows = mysqli_fetch_assoc($query_result)){ //fetches rows(if any)from query object one after another.
+            foreach($rows as $row){
+                echo $row,'<br>';
+            }
+        };
     }
-    catch(mysqli_sql_exception){
-        echo 'Could not register user.<br>';
-    }
-
+    else{
+        echo 'No User Found';
+    };
+    
+    
     mysqli_close($connection);
 
 ?>
